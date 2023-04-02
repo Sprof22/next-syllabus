fa54208f1cea8f7f3acc5d0fcbcb6badec536a16
Sprof22 <richmondelaigwu@gmail.com>
2023-04-02 09:42:18 +0100

    commit please

---
+++ src/data/auth/index.ts
@@ -0,0 +1,42 @@
+export interface User {
+    name: string;
+    photo: string;
+}
+
+export interface Access {
+    accessToken: string
+    expiresIn: number;
+    granted: boolean;
+    timestamp: number;
+    refreshToken: string;
+}
+
+
+export interface LoginInterface {
+    email: string;
+    username: string;
+    password: string;
+}
+
+export interface ISignupInterface extends LoginInterface {
+    companyName: string;
+}
+
+
+export interface authStore {
+    access?: Access;
+    user?: User;
+}
+
+export interface PartialUpdate {
+    user: Partial<User>
+}
+
+
+export interface IAuthStore extends authStore {
+    setUser: (payload: User) => void
+    setAccess: (payload: Access) => void
+    Login: (payload: LoginInterface) => Promise<void>
+    Signup: (payload: ISignupInterface) => Promise<void>
+    logoutAccount: () => Promise<void>
+}

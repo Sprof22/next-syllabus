e59e0601182323f38ff9f39158445bf6561fae64
Sprof22 <richmondelaigwu@gmail.com>
2023-04-15 13:58:40 +0100

    ISR

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,13 +1,38 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-const ProductDetails = () => {
+const ProductDetails = ({product}) => {
     const router = useRouter();
-    const productID = router.query.productId
-    console.log(router, "this is the router")
+
+    if(router.isFallback){
+      return <div>Loading...</div>
+    }
   return (
-    <div><h1>Details about product {productID} </h1></div>
+    <div><h2>{product.productId} {product.title} {product.price}</h2>
+    <hr/><p>{product.description}</p></div>
   )
 }
 
 export default ProductDetails
+
+export async function getStaticProps(context){
+  const {params} = context
+  const response = await fetch(`http://localhost:3004/products/${params.productId}`);
+  const data = await response.json()
+
+  return {
+    props: {
+      product: data
+    }
+  }
+
+}
+
+export async function getStaticPaths(){
+  const response = await fetch("http://localhost:3004/products");
+  const data = await response.json();
+  return{
+    paths: [{params: {productId : "1"}}],
+    fallback: true,
+  }
+} 
